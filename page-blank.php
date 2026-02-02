2b070c0b991882f2a297feeb177e47682dda938f
Gabrielb102 <gabrielb102@gmail.com>
2026-02-01 23:14:07 -0500

    Fixed mobile view with viewpo[rt meta tag

--- page-blank.php
+++ page-blank.php
@@ -6,6 +6,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo('charset'); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class('app-blank'); ?>>
